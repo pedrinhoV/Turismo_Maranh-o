a845b4ee02a8e1685900ea1195500eaabf27cb51
pedrinhoV <165428109+pedrinhoV@users.noreply.github.com>
2024-07-26 01:53:31 -0300

    Update routes.js
    adicionando as rotas referentes aos novos controllers

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const DestinoController = require('./controllers/DestinoController');
 
+const AtrativoController = require('./controllers/AtrativoController');
+
 const router = express.Router();
 
 router.get('/destinos', (DestinoController.pegaTodosDestinos));
 
 router.get('/destinos/:nome', (DestinoController.pegaDestino));
+
+router.get('/atrativos', (AtrativoController.pegaTodosAtrativos));
+
+router.get('/atrativos/:nome', (AtrativoController.pegaAtrativo));
+
+router.get('/destinos/atrativos/:municipio', (AtrativoController.pegaAtrativoPorLocal));
 
 module.exports = router;
