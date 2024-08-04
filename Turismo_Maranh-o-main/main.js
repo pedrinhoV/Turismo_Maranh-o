cd851eac593a121110ba740d896e501345ecc3c1
pedrinhoV <165428109+pedrinhoV@users.noreply.github.com>
2024-08-04 20:26:56 -0300

    Delete Turismo_Maranh-o-main directory

--- Turismo_Maranh-o-main/main.js
+++
@@ -1,8 +0,0 @@
-const attractionsButton = document.querySelector('.attractions');
-const botaolink = document.getElementById('link');
-
-function updateAttractionsButton(cidade) {
-  attractionsButton.dataset.id = `attraction-${cidade}`;
-  const link = `attractions.html?attractionId=attraction-${cidade}`;
-  botaolink.href = link;
-;}
