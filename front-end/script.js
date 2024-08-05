c851e8618d0fefd5206644a47a175cdefc9fa4c6
pedrinhoV <165428109+pedrinhoV@users.noreply.github.com>
2024-08-05 10:22:45 -0300

    Add files via upload

---
+++ front-end/script.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const card1 = document.querySelector('.card1');
+    const card2 = document.querySelector('.card2');
+    const card3 = document.querySelector('.card3');
+    const card4 = document.querySelector('.card4');
+    const bg = document.querySelector('.general');
+    const title = document.querySelector('h1');
+    const textCard = document.querySelector('p');
+    const visitButton = document.querySelector('.visit');
+    const attractionsButton = document.querySelector('.attractions');
+    const mapLencois = document.querySelector('.map-lencois');
+    const mapSaoLuis = document.querySelector('.map-sao-luis');
+    const mapChapada = document.querySelector('.map-chapada');
+    const mapAlcantara = document.querySelector('.map-alcantara');
+    const cancelButton = document.querySelector('.cancel');
+    const container = document.querySelector('.container');
+    
+    // Ocultar todos os mapas
+    function hideAllMaps() {
+        mapLencois.classList.remove('show');
+        mapSaoLuis.classList.remove('show');
+        mapChapada.classList.remove('show');
+        mapAlcantara.classList.remove('show');
+    }
+
+    // Mostrar os botões visitar e atrações
+    function showButtons() {
+        visitButton.style.display = 'block';
+        attractionsButton.style.display = 'block';
+    }
+
+    // Ocultar os botões visitar e atrações
+    function hideButtons() {
+        visitButton.style.display = 'none';
+        attractionsButton.style.display = 'none';
+    }
+
+    // Restaurar a página aos padrões iniciais
+    function resetPage() {
+        bg.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('./img/maranhao.jpg')";
+        title.innerText = 'Trilhas - TourMA';
+        textCard.innerText = 'Trilhas: TourMA é uma inovadora iniciativa voltada para a promoção e facilitação do turismo no território maranhense...';
+        hideAllMaps();
+        hideButtons();
+        cancelButton.style.display = 'none';
+        container.style.display = 'flex';
+    }
+    // Ocultar o botão cancel quando qualquer outro card for clicado
+    function hideCancelButton() {
+        cancelButton.style.display = 'none';
+    }
+
+    // Evento de clique para cada card
+    function addCardClickListener(card, bgImage, titleText, descriptionText, visitButtonId, attractionId) {
+        card.addEventListener('click', () => {
+            bg.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),url(${bgImage})`;
+            title.innerText = titleText;
+            textCard.innerText = descriptionText;
+            visitButton.id = visitButtonId;
+            attractionsButton.id = attractionId;
+            attractionsButton.href = `attractions.html?attractionId=${attractionId}`;
+            hideAllMaps();
+            showButtons();
+            cancelButton.style.display = 'none'; 
+        });
+    }
+
+    fetch('https://api-desafio-5-turismo.onrender.com/tudo/destinos').then(res => res.json()).then((dados) => {
+        addCardClickListener(card1, './img/barreirinhas-background.png', 'Barreirinhas', dados[2].descricao , 'visit-lencois');
+        addCardClickListener(card2, './img/centro-historico-sao-luis.jpg', 'São Luís', dados[0].descricao, 'visit-sao-luis');
+        addCardClickListener(card3, './img/carolina-background.png', 'Carolina', dados[1].descricao, 'visit-chapada');
+        addCardClickListener(card4, './img/alcantara-background.png', 'Alcântara', dados[3].descricao, 'visit-alcantara');
+    })
+
+    // Mostrar mapa e button.cancel
+    visitButton.addEventListener('click', () => {
+        if (visitButton.id === 'visit-sao-luis') {
+            mapSaoLuis.classList.add('show');
+        } else if (visitButton.id === 'visit-lencois') {
+            mapLencois.classList.add('show');
+        } else if (visitButton.id === 'visit-chapada') {
+            mapChapada.classList.add('show');
+        } else if (visitButton.id === 'visit-alcantara') {
+            mapAlcantara.classList.add('show');
+        }
+        cancelButton.style.display = 'block';
+    });
+
+    // Ocultar o mapa -> button.cancel
+    cancelButton.addEventListener('click', () => {
+        hideAllMaps();
+        cancelButton.style.display = 'none';
+    });
+});
